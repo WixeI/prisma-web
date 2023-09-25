919112636c1e63de02de386699cb44348dbc8aa6
Paulo Martins <pauloportelamartins@gmail.com>
2023-09-25 10:31:33 -0300

    feat: added Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,30 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Script from "next/script";
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <>
+      {/* Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src="www.googletagmanager.com/gtag/js?id=G-8GKTC76PDN"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-8GKTC76PDN');
+        `}
+      </Script>
+
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </>
   );
 }
